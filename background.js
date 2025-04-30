b7ff057c560a852fd0a638ced18e4322bf5b809a
davidpuijn <141274783+davidpuijn@users.noreply.github.com>
2025-05-01 01:53:52 +0200

    Add files via upload

---
+++ background.js
@@ -0,0 +1,31 @@
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  if (msg.action === 'aiResult') {
+    chrome.storage.local.get(['siteDB'], res => {
+      const siteDB = res.siteDB || {};
+      siteDB[msg.url] = msg.status;
+      chrome.storage.local.set({ siteDB }, () => {
+        console.log("AI result opgeslagen:", msg.url, msg.status);
+      });
+    });
+  }
+
+  if (msg.action === 'markSite') {
+    chrome.storage.local.get(['siteDB'], res => {
+      const siteDB = res.siteDB || {};
+      siteDB[msg.url] = msg.status;
+      chrome.storage.local.set({ siteDB }, () => {
+        console.log("Handmatig gemarkeerd:", msg.url, msg.status);
+      });
+    });
+
+    // Extra: stuur markering ook naar backend (eventueel met leerdata)
+    fetch("https://dropshipping-backend.onrender.com/report", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": "MY_SECRET_API_KEY"
+      },
+      body: JSON.stringify({ url: msg.url, status: msg.status })
+    });
+  }
+});
